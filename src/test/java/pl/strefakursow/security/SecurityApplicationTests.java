9b2026c1df309274f88d942262c54be2b5191afa
Mike Wojtyna <mike@buildmysoftware.pro>
2020-01-28 01:45:41 +0100

    Create project

---
+++ src/test/java/pl/strefakursow/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.strefakursow.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
