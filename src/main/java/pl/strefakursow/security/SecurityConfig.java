29db8c8961d5038e05d97a57e60f3de0ffc44460
Mike Wojtyna <mike@buildmysoftware.pro>
2020-01-28 17:15:02 +0100

    Add always allow filter example

--- src/main/java/pl/strefakursow/security/SecurityConfig.java
+++ src/main/java/pl/strefakursow/security/SecurityConfig.java
@@ -1,32 +1,42 @@
 package pl.strefakursow.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-
-import java.util.List;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextHolderStrategy;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.antMatcher("/**").authorizeRequests().anyRequest()
-			.authenticated().and().formLogin();
-	}
+	@Autowired
+	private AlwaysAllowFilter alwaysAllowFilter;
 
 	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.antMatcher("/**").addFilterAfter(alwaysAllowFilter,
+			BasicAuthenticationFilter.class).authorizeRequests()
+			.anyRequest().authenticated().and().formLogin();
+	}
+
+	@Configuration
+	public static class SecurityContextHolderConfig {
+		@Bean
+		public SecurityContextHolderStrategy securityContextHolderStrategy() {
+			return SecurityContextHolder.getContextHolderStrategy();
+		}
+	}
+
+	/*@Override
 	protected void configure(
 		AuthenticationManagerBuilder auth) throws Exception {
 		auth.authenticationProvider(new AuthenticationProvider() {
 			@Override
 			public Authentication authenticate(
-				Authentication authentication) throws AuthenticationException {
+				Authentication authentication) throws
+				AuthenticationException {
 				return new UsernamePasswordAuthenticationToken(
 					"any principal", "any credentials",
 					List.of(new SimpleGrantedAuthority(
@@ -38,5 +48,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				return true;
 			}
 		});
-	}
+	}*/
 }
